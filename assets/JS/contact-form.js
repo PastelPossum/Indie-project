0ec913f8c6f3fa7993f97f55c6c0d803d6bdf81e
PastelPossum <iggyskywalker@gmail.com>
2022-12-05 10:58:58 +0100

    form validering

---
+++ assets/JS/contact-form.js
@@ -0,0 +1,29 @@
+const inputName = document.querySelector("#name")
+const inputEmail = document.querySelector("#email")
+const inputMessage = document.querySelector("#message")
+const submitBtn = document.querySelector("#submit")
+const errorMsg = document.querySelector("#error-msg")
+
+submitBtn.addEventListener("click", function(event){
+
+    event.preventDefault()
+
+    if (inputName.value == "") {
+
+        errorMsg.innerHTML = "Indtast venligst dit navn"
+
+    } else if (inputEmail.value == "") {
+
+        errorMsg.innerHTML = "Indtast venligst din email"
+
+    } else if (inputMessage.value == "") {
+
+        errorMsg.innerHTML = "Skriv venlist en besked i tekstfeltet"
+
+    } else {
+
+        window.location.reload();
+
+    }
+
+})
